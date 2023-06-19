ab7debf225336b1c92d15bafb34d3c0e4174fc3f
pavv3l <pavv3l@gmail.com>
2023-06-19 21:38:28 +0200

    Update dev_pico.h
    comeback after  long time

--- pico_driver/dev_pico.h
+++ pico_driver/dev_pico.h
@@ -8,6 +8,7 @@ extern "C" {
 #include <stdio.h>
 #include "string.h"
 #include "hardware/i2c.h"
+#include "hardware/spi.h"
 #include "bme280.h"
 
 #define I2C_PORT_0 i2c0
@@ -25,11 +26,16 @@ typedef struct identifier
 {
     uint8_t dev_addr;
     i2c_inst_t* i2c_ptr;
+    spi_inst_t* spi_ptr;
 } identifier_t;
 
 void user_delay_us(uint32_t period, void *intf_ptr);
-int8_t user_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr);
-int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr);
+
+int8_t user_i2c_read(uint8_t reg_addr, uint8_t* data, uint32_t len, void* intf_ptr);
+int8_t user_i2c_write(uint8_t reg_addr, const uint8_t* data, uint32_t len, void* intf_ptr);
+
+int8_t user_spi_read(uint8_t reg_addr, uint8_t* data, uint32_t len, void* intf_ptr);
+int8_t user_spi_write(uint8_t reg_addr, const uint8_t* data, uint32_t len, void* intf_ptr);
 
 #ifdef __cplusplus
 }
